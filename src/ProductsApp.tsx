17bcfe5feb56a5a3b7ecd5a440585994294a2773
EstebanMacbook <macbook@Esteban-MacBook-Pro.local>
2024-03-09 13:02:58 -0500

    finished login screen ui

--- src/ProductsApp.tsx
+++ src/ProductsApp.tsx
@@ -1,11 +1,40 @@
-import { View, Text } from 'react-native';
+import 'react-native-gesture-handler';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import StackNavigator from './presentation/navigation/StackNavigator';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
+import { useColorScheme } from 'react-native';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
 
 const ProductsApp = () => {
+  const colorScheme = useColorScheme();
+  const theme = colorScheme === 'dark' ? eva.dark : eva.light;
+  const backgroundColor =
+    colorScheme === 'dark'
+      ? theme['color-basic-800']
+      : theme['color-basic-100'];
+
   return (
-    <View>
-      <Text>ProductsApp</Text>
-    </View>
+    <>
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider {...eva} theme={theme}>
+        <NavigationContainer
+          theme={{
+            dark: colorScheme === 'dark',
+            colors: {
+              primary: theme['color-primary-500'],
+              background: backgroundColor,
+              card: theme['color-basic-100'],
+              text: theme['color-basic-color'],
+              border: theme['color-basic-800'],
+              notification: theme['color-primary-500'],
+            },
+          }}>
+          <StackNavigator />
+        </NavigationContainer>
+      </ApplicationProvider>
+    </>
   );
 };
 
