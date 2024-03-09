09baf96d4b49175d75507117fe4e515ba71bb8f2
EstebanMacbook <macbook@Esteban-MacBook-Pro.local>
2024-03-09 10:47:27 -0500

    added initial file structure to the project

---
+++ src/ProductsApp.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const ProductsApp = () => {
+  return (
+    <View>
+      <Text>ProductsApp</Text>
+    </View>
+  );
+};
+
+export default ProductsApp;
