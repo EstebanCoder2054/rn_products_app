271a7aaf4e312917ed31440a9b84064e5d8ab9de
EstebanMacbook <macbook@Esteban-MacBook-Pro.local>
2024-06-08 20:24:17 -0500

    infinite scroll and pull to refresh done

--- src/presentation/components/products/ProductList.tsx
+++ src/presentation/components/products/ProductList.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Product } from '../../../domain/entities/product';
 import { Layout, List, Text } from '@ui-kitten/components';
 import ProductCard from './ProductCard';
+import { RefreshControl } from 'react-native';
 
 interface Props {
   products: Product[];
+  fetchNextPage: () => void;
   // TODO: fetch next page
 }
 
-const ProductList = ({ products }: Props) => {
+const ProductList = ({ products, fetchNextPage }: Props) => {
+  const [isRefreshing, setIsRefreshing] = useState(false);
+
+  const onPullToRefresh = async () => {
+    setIsRefreshing(true);
+    // Sleep 1.5 secs
+    await new Promise(resolve => setTimeout(resolve, 1500));
+
+    setIsRefreshing(false);
+  };
+
   return (
     <List
       data={products}
@@ -16,6 +28,11 @@ const ProductList = ({ products }: Props) => {
       keyExtractor={(item, index) => `${item.id}-${index}`}
       renderItem={({ item }) => <ProductCard product={item} />}
       ListFooterComponent={() => <Layout style={{ height: 150 }} />}
+      onEndReached={fetchNextPage}
+      onEndReachedThreshold={0.8}
+      refreshControl={
+        <RefreshControl refreshing={isRefreshing} onRefresh={onPullToRefresh} />
+      }
     />
   );
 };
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import { getProductsByPage } from '../../../actions/products/get-products-by-page';
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 import MainLayout from '../../layouts/MainLayout';
 import FullScreenLoader from '../../components/ui/FullScreenLoader';
 import ProductList from '../../components/products/ProductList';
 
 const HomeScreen = () => {
-  const { isLoading, data: products = [] } = useQuery({
+  // const { isLoading, data: products = [] } = useQuery({
+  //   queryKey: ['products', 'infinite'],
+  //   staleTime: 1000 * 60 * 60, // 1 hour
+  //   queryFn: () => getProductsByPage(0),
+  // });
+
+  const { isLoading, data, fetchNextPage } = useInfiniteQuery({
     queryKey: ['products', 'infinite'],
     staleTime: 1000 * 60 * 60, // 1 hour
-    queryFn: () => getProductsByPage(0),
+    initialPageParam: 0,
+    queryFn: async params => {
+      console.log({ params });
+      return await getProductsByPage(params.pageParam);
+    },
+    getNextPageParam: (lastPage, allPages) => allPages.length,
   });
 
   return (
@@ -19,7 +30,14 @@ const HomeScreen = () => {
       // rightAction={() => {}}
       // rightActionIcon="plus-outline"
     >
-      {isLoading ? <FullScreenLoader /> : <ProductList products={products} />}
+      {isLoading ? (
+        <FullScreenLoader />
+      ) : (
+        <ProductList
+          products={data?.pages.flat() ?? []}
+          fetchNextPage={fetchNextPage}
+        />
+      )}
     </MainLayout>
   );
 };
